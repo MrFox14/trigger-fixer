3411a54651f8b7ea742a41e701a0cead2d8fd92e
EchoDevG <75668023+EchoDevG@users.noreply.github.com>
2021-06-12 22:52:16 +0100

    Add files via upload

--- content_script.js
+++ content_script.js
@@ -1,148 +1,61 @@
 var elements = document.getElementsByTagName('*');
 
-var replaceThis1;
-var replaceThis2;
-var replaceThis3;
-var replaceThis4;
-var replaceThis5;
+var replaceThisWord = "";
+var replaceWithWord = "";
 
-var replaceWith1;
-var replaceWith2;
-var replaceWith3;
-var replaceWith4;
-var replaceWith5;
+var replaceThisCommaDel;
+var replaceWithCommaDel;
+
+
 
 chrome.storage.sync.get([
- 'replaceThis1',
- 'replaceThis2',
- 'replaceThis3',
- 'replaceThis4',
- 'replaceThis5',
- 'replaceWith1',
- 'replaceWith2',
- 'replaceWith3',
- 'replaceWith4',
- 'replaceWith5'
+ 'replaceThis',
+ 'replaceWith'
 ], function(result) {
 	
-	replaceThis1=result.replaceThis1;
-	replaceThis2=result.replaceThis2;
-	replaceThis3=result.replaceThis3;
-	replaceThis4=result.replaceThis4;
-	replaceThis5=result.replaceThis5;
+	if (result.replaceThis !== undefined){replaceThisCommaDel = result.replaceThis};
+	if (result.replaceWith !== undefined){replaceWithCommaDel = result.replaceWith};
+
+	replaceThisCommaDel = replaceThisCommaDel.substring(1);
+	replaceWithCommaDel = replaceWithCommaDel.substring(1);
 	
-	replaceWith1=result.replaceWith1;
-	replaceWith2=result.replaceWith2;
-	replaceWith3=result.replaceWith3;
-	replaceWith4=result.replaceWith4;
-	replaceWith5=result.replaceWith5;
+	replaceThisCommaDel = replaceThisCommaDel.slice(0, -1);
+	replaceWithCommaDel = replaceWithCommaDel.slice(0, -1);
+
+	var replaceThisArr = replaceThisCommaDel.split(',');
+	var replaceWithArr = replaceWithCommaDel.split(',');
 	
-	re1 = new RegExp(`\\b${replaceThis1}\\b`, 'i');
-	re2 = new RegExp(`\\b${replaceThis2}\\b`, 'i');
-	re3 = new RegExp(`\\b${replaceThis3}\\b`, 'i');
-	re4 = new RegExp(`\\b${replaceThis4}\\b`, 'i');
-	re5 = new RegExp(`\\b${replaceThis5}\\b`, 'i');
-
-
-if (replaceThis1 !== ""){
-	for (var i = 0; i < elements.length; i++) {
-			var element = elements[i];
-
-		for (var j = 0; j < element.childNodes.length; j++) {
-			var node = element.childNodes[j];
-
-			if (node.nodeType === 3) {
-				var text = node.nodeValue;
-				var replacedText = text.replace(re1, String(replaceWith1));
-
-				if (replacedText !== text) {
-					element.replaceChild(document.createTextNode(replacedText), node);
-				}
-			}
-		}
-	}
-}
-
-if (replaceThis2 !== ""){
-	for (var i = 0; i < elements.length; i++) {
-		var element = elements[i];
+	var length = (replaceThisArr.length);
 	
-		for (var j = 0; j < element.childNodes.length; j++) {
-			var node = element.childNodes[j];
+for (i = 1; i < length; i++) {
+	
+	replaceThisWord = replaceThisArr[i];
+	replaceWithWord = replaceWithArr[[i]];
+	replaceWords(replaceThisWord, replaceWithWord);
+}
 
-			if (node.nodeType === 3) {
-				var text = node.nodeValue;
-				var replacedText = text.replace(re2, String(replaceWith2));
 
-				if (replacedText !== text) {
-					element.replaceChild(document.createTextNode(replacedText), node);
+function replaceWords(replaceThisText, ReplaceWithText) {
+	re = new RegExp(`\\b${replaceThisText}\\b`, 'i');
+	if (replaceThisText !== ""){
+		for (var i = 0; i < elements.length; i++) {
+				var element = elements[i];
+
+			for (var j = 0; j < element.childNodes.length; j++) {
+				var node = element.childNodes[j];
+
+				if (node.nodeType === 3) {
+					var text = node.nodeValue;
+					var replacedText = text.replace(re, String(ReplaceWithText));
+
+					if (replacedText !== text) {
+						element.replaceChild(document.createTextNode(replacedText), node);
+					}
 				}
 			}
 		}
 	}
 }
-
-if (replaceThis3 !== ""){
-	for (var i = 0; i < elements.length; i++) {
-		var element = elements[i];
-
-		for (var j = 0; j < element.childNodes.length; j++) {
-			var node = element.childNodes[j];
-
-			if (node.nodeType === 3) {
-				var text = node.nodeValue;
-				var replacedText = text.replace(re3, String(replaceWith3));
-
-				if (replacedText !== text) {
-					element.replaceChild(document.createTextNode(replacedText), node);
-				}
-			}
-		}
-	}
-}
-
-
-if (replaceThis4 !== ""){
-	for (var i = 0; i < elements.length; i++) {
-		var element = elements[i];
-
-		for (var j = 0; j < element.childNodes.length; j++) {
-			var node = element.childNodes[j];
-
-			if (node.nodeType === 3) {
-				var text = node.nodeValue;
-				var replacedText = text.replace(re4, String(replaceWith4));
-
-				if (replacedText !== text) {
-					element.replaceChild(document.createTextNode(replacedText), node);
-				}
-			}
-		}
-	}
-}
-
-
-if (replaceThis5 !== ""){
-	for (var i = 0; i < elements.length; i++) {
-		var element = elements[i];
-
-		for (var j = 0; j < element.childNodes.length; j++) {
-			var node = element.childNodes[j];
-
-			if (node.nodeType === 3) {
-				var text = node.nodeValue;
-				var replacedText = text.replace(re5, String(replaceWith5));
-
-				if (replacedText !== text) {
-					element.replaceChild(document.createTextNode(replacedText), node);
-				}
-			}
-		}
-	}
-}
-
-
-
 
 });
 
--- options.js
+++ options.js
@@ -1,42 +1,48 @@
 
+var replaceThisCommaDel = ",";
+var replaceWithCommaDel = ",";
 
 
-document.getElementById("submit").addEventListener("click", submitWords);
+chrome.storage.sync.get([
+    'replaceThis',
+    'replaceWith'
+], function(result) {
 
-
-function submitWords(){
-  console.log('asd');
-
-
-
-	var replaceThis1 = document.getElementById("replaceThis1").value;
-	var replaceWith1 = document.getElementById("replaceWith1").value;
+    if (result.replaceThis !== undefined){replaceThisCommaDel = String(result.replaceThis)};
+		
+    if (result.replaceWith !== undefined){replaceWithCommaDel = String(result.replaceWith)};
 	
-	var replaceThis2 = document.getElementById("replaceThis2").value;
-	var replaceWith2 = document.getElementById("replaceWith2").value;
 	
-	var replaceThis3 = document.getElementById("replaceThis3").value;
-	var replaceWith3 = document.getElementById("replaceWith3").value;
-	
-	var replaceThis4 = document.getElementById("replaceThis4").value;
-	var replaceWith4 = document.getElementById("replaceWith4").value;
-	
-	var replaceThis5 = document.getElementById("replaceThis5").value;
-	var replaceWith5 = document.getElementById("replaceWith5").value;
-	
-	console.log(replaceThis1);
-	console.log(replaceThis2);
-	
-	chrome.storage.sync.set({'replaceThis1': replaceThis1})
-	chrome.storage.sync.set({'replaceThis2': replaceThis2})
-	chrome.storage.sync.set({'replaceThis3': replaceThis3})
-	chrome.storage.sync.set({'replaceThis4': replaceThis4})
-	chrome.storage.sync.set({'replaceThis5': replaceThis5})
-	
-	chrome.storage.sync.set({'replaceWith1': replaceWith1})
-	chrome.storage.sync.set({'replaceWith2': replaceWith2})
-	chrome.storage.sync.set({'replaceWith3': replaceWith3})
-	chrome.storage.sync.set({'replaceWith4': replaceWith4})
-	chrome.storage.sync.set({'replaceWith5': replaceWith5})
+    console.log(replaceThisCommaDel);
+    console.log(replaceWithCommaDel);
 
-}
+
+    document.getElementById("submit").addEventListener("click", submitClicked);
+
+
+    function submitClicked() {
+
+        var replaceThis = String(document.getElementById("replaceThis").value);
+        var replaceWith = String(document.getElementById("replaceWith").value);
+
+
+        console.log(replaceThis);
+        console.log(replaceWith);
+
+        replaceThisCommaDel = replaceThisCommaDel + replaceThis + ",";
+        replaceWithCommaDel = replaceWithCommaDel + replaceWith + ",";
+
+        console.log(replaceThisCommaDel);
+        console.log(replaceWithCommaDel);
+
+        chrome.storage.sync.set({
+            'replaceThis': replaceThisCommaDel
+        })
+        chrome.storage.sync.set({
+            'replaceWith': replaceWithCommaDel
+        })
+
+
+
+    }
+});
